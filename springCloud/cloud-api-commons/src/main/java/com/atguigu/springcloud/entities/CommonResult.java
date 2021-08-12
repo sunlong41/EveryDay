62c73e8a7c3250d9793e555ffb8023a2ca6f7e2f
sunlong <736984575@qq.com>
2021-08-12 10:31:57 +0800

    OpenFen的第一次尝试

--- springCloud/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ springCloud/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -1,12 +1,7 @@
 package com.atguigu.springcloud.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class CommonResult<T> {
     private Integer code;
     private String message;
@@ -15,4 +10,36 @@ public class CommonResult<T> {
         this(code,message,null);
     }
 
+    public CommonResult(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public CommonResult() {
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
 }
--- springCloud/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
+++ springCloud/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -1,13 +1,31 @@
 package com.atguigu.springcloud.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Payment {
     private Long id;
     private String serial;
+
+    public Payment(Long id, String serial) {
+        this.id = id;
+        this.serial = serial;
+    }
+
+    public Payment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
 }
